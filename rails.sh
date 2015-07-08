e2909b58897ca3ebc3bed26b10aec9132e400e64
unmanbearpig <unmanbearpig@gmail.com>
2015-07-08 05:36:14 +0300

    rnew command for new rails app

--- rails.sh
+++ rails.sh
@@ -12,3 +12,13 @@ function rt {
 
 alias sr="spring rspec"
 alias te="t && e"
+
+export RAILS_TEMPLATE="~/projects/rails/rails_templates/unmbp.rb"
+
+function rnew {
+    project_path=$1
+    project_name=`basename $1`
+    cd `dirname $project_path`
+    rails new $project_name --template=$RAILS_TEMPLATE --skip-test-unit
+    cd $project_name
+}
