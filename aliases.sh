972e2e5c724d7afec70c1e6d4bbc6d61869124c4
unmanbearpig <unmanbearpig@gmail.com>
2016-06-04 12:20:22 +0700

    add 'la = ls -a' alias

--- aliases.sh
+++ aliases.sh
@@ -61,3 +61,4 @@ function md {
 alias migrate="rake db:migrate db:test:prepare"
 alias remigrate="rake db:migrate && rake db:migrate:redo && rake db:schema:dump db:test:prepare"
 alias pk="pkill"
+alias la="ls -a"
